cfa8cf86b7ba1889f75d9920853f6e570e6e009e
Anieb22 <anielloverdino@gmail.com>
2023-10-15 16:29:37 +0200

    prova funzioni

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-const enum Size { Small = 1, Medium, Large };
-let mySize: Size = Size.Medium;
+function calculateTax(income: number, taxYear = 2023): number {
+    if (taxYear < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
 
-console.log(mySize);
+calculateTax(10_000)
