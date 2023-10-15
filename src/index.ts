91bc8a12ea6dc810b007698e7dddcff1f99b0e8e
Anieb22 <anielloverdino@gmail.com>
2023-10-15 16:46:13 +0200

    prova Unions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-type Employee = {
-    readonly id: number,
-    name: string,
-    retire: (date: Date)
-        => void
-}
-
-let employee: Employee = {
-    id: 1,
-    name: 'Aniello',
-    retire: (date: Date) => {
-        console.log(date);
+function kgToLbs(weight: number | string): number {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else {
+        return parseInt(weight) * 2.2;
     }
 }
+
+kgToLbs(10);
+kgToLbs('10kg')
